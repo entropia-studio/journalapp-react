c8dd3841b9a264bef397d118fa728dcd7446770c
Javier Sánchez <javier.sanchezortiz@xploregroup.es>
2022-08-08 09:39:19 +0200

    feat: Create routing and basic components

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import { AppRouter } from './router/AppRouter';
 
 export const JournalApp = () => {
   return (
     <>
-      <h1>JournalApp</h1>
+      <AppRouter />
     </>
   );
 };
