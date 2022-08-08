60eae3289125a6188a5904547e276d15f0716567
Javier Sánchez <javier.sanchezortiz@xploregroup.es>
2022-08-08 11:37:15 +0200

    feat: LoginPage without layout

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,3 +1,49 @@
+import { Grid, Typography, TextField } from '@mui/material';
+
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction='column'
+      aligItems='center'
+      justifyContent='center'
+      sx={{
+        minHeight: '100vh',
+        backgroundColor: 'primary.main',
+        padding: 4,
+      }}
+    >
+      <Grid
+        item
+        className='box-shadow'
+        xs={3}
+        sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2 }}
+      >
+        <Typography variant='h5' sx={{ mb: 1 }}>
+          Login
+        </Typography>
+        <form>
+          <Grid container>
+            <Grid item xs={12} sx={{ marginTop: 2 }}>
+              <TextField
+                label='Email'
+                type='email'
+                placeholder='john@doe.com'
+                fullWidth
+              ></TextField>
+            </Grid>
+            <Grid item xs={12} sx={{ marginTop: 2 }}>
+              <TextField
+                label='Password'
+                type='password'
+                placeholder='password'
+                fullWidth
+              ></TextField>
+            </Grid>
+          </Grid>
+        </form>
+      </Grid>
+    </Grid>
+  );
 };
