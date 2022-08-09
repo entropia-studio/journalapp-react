f7ba8d2172b5ed3de55a109d29ed9e78cbd86e40
Javier Sánchez <javier.sanchezortiz@xploregroup.es>
2022-08-09 09:34:33 +0200

    feat: JournalLayout and JournalPage

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,5 +1,17 @@
 import { Typography } from '@mui/material';
+import { JournalLayout } from '../layout/JournalLayout';
 
 export const JournalPage = () => {
-  return <Typography variant='h1'>JournalPage</Typography>;
+  return (
+    <JournalLayout>
+      <Typography>
+        Ipsum nisi irure sit sit nostrud ea aliqua velit. Labore fugiat
+        consectetur culpa laboris Lorem nisi. Minim laboris aliqua duis occaecat
+        commodo dolor dolor nulla consequat elit enim anim non. Ex tempor non ea
+        qui non dolore veniam ut et laborum.
+      </Typography>
+      {/* NothingSelected */}
+      {/* Not view */}
+    </JournalLayout>
+  );
 };
